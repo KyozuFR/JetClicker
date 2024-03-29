4306c940dafdf30c6f71a8fafa358103b091597a
Menard <ludomen64@gmail.com>
2024-03-29 16:41:40 +0100

    version 1.0 de la fonction avion

--- app/src/main/java/jetclicker/App.java
+++ app/src/main/java/jetclicker/App.java
@@ -10,5 +10,6 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        avion AvionTest = new avion(true, "Gerar", 15, 180, 5.0, 2.0);
     }
 }
