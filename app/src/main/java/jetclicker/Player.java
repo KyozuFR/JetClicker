895f5660c4178de9df2f2ed7748b21c24bc22fa5
Colas <colas.michard--melon@etudiant.univ-lr.fr>
2024-04-05 15:34:42 +0200

    La classe Player qui contient les scores et le nom

---
+++ app/src/main/java/jetclicker/Player.java
@@ -0,0 +1,20 @@
+class Player{
+    public Integer score;
+    public Stirng nom;
+
+    public void scoreUp(){
+        this.score=this.score+1;
+    }
+
+    public getScore(){
+        return this.score;
+    }
+
+    public void setNom(String nom){
+        this.nom=nom;
+    }
+
+    public getNom(){
+        return this.nom;
+    }
+}
