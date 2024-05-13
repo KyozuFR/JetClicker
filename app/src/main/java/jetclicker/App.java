a83d22eac4d8d800b4c5e266aa54299c263744df
Amaury Mulcey <amaury.mulcey@etudiant.univ-lr.fr>
2024-05-13 14:08:51 +0200

    Implémentation de l'api d'OpenSkyNetwork au projet

--- app/src/main/java/jetclicker/App.java
+++ app/src/main/java/jetclicker/App.java
@@ -3,6 +3,13 @@
  */
 package jetclicker;
 
+import java.io.IOException;
+
+import jetclicker.opensky.api.OpenSkyApi;
+import jetclicker.opensky.api.OpenSkyApi.BoundingBox;
+import jetclicker.opensky.model.OpenSkyStates;
+import jetclicker.opensky.model.StateVector;
+
 public class App {
 
 
@@ -13,12 +20,12 @@ public class App {
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
 
-        Fenetre tab = new Fenetre();
-        tab.setVisible(false); //pour actualiser la fenetre et avoir les limite de l'écran actualisé (c'est du bricolage)
-        tab.setVisible(true);
+        //Fenetre tab = new Fenetre();
+        //tab.setVisible(false); //pour actualiser la fenetre et avoir les limite de l'écran actualisé (c'est du bricolage)
+        //tab.setVisible(true);
 
-        Thread Tick = new Thread(new Tick());
-        Tick.start();
+        //Thread Tick = new Thread(new Tick());
+        //Tick.start();
 
         //Avion AvionTest = new Avion(true, "Gerar", Math.PI, 10, 0.0, 0.0);
         //AvionTest.deplacement();
@@ -28,5 +35,20 @@ public class App {
 
         //Liste_Avion liste_AvionTest = new Liste_Avion();
         //liste_AvionTest.bouger_Avions();
+
+        try {
+            OpenSkyApi api = new OpenSkyApi("Dystog", "@JetClicker02");
+            OpenSkyStates os = api.getStates(0, null, new BoundingBox(45.8389, 47.8229, 5.9962, 10.5226)); // Utilisation directe de BoundingBox
+            if (os.getStates() != null) {
+                System.out.println("Nombre d'états récupérés : " + os.getStates().size());
+                for (StateVector state : os.getStates()) {
+                    System.out.println("ICAO24 : " + state.getIcao24() + " CALLSIGN : " + state.getCallsign());
+                }
+            } else {
+                System.out.println("Aucune donnée d'état retournée.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
