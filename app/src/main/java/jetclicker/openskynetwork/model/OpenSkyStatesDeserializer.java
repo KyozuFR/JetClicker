d2c42a79bf3f620d2eb03ff6cd91f2d47c0602a4
Amaury Mulcey <amaury.mulcey@etudiant.univ-lr.fr>
2024-05-15 01:12:00 +0200

    update du dossier, pas de gros changement

--- app/src/main/java/jetclicker/openskynetwork/model/OpenSkyStatesDeserializer.java
+++ app/src/main/java/jetclicker/openskynetwork/model/OpenSkyStatesDeserializer.java
@@ -85,7 +85,10 @@ public class OpenSkyStatesDeserializer extends StdDeserializer<OpenSkyStates> {
 		return result;
 	}
 
+<<<<<<< HEAD
 	@SuppressWarnings("deprecation")
+=======
+>>>>>>> 185858f (update du dossier, pas de gros changement)
 	@Override
 	public OpenSkyStates deserialize(JsonParser jp, DeserializationContext dc) throws IOException {
 		if (jp.getCurrentToken() != null && jp.getCurrentToken() != JsonToken.START_OBJECT) {
